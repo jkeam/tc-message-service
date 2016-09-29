5e631fb195ea2b9ec7c58b306a465fc451f8959e
Mauricio Desiderio <mdesiderio@appirio.com>
2016-09-29 17:04:45 -0500

    supporting list of admin users to map to system in discourse

--- app/middleware/system-user-filter.js
+++ app/middleware/system-user-filter.js
@@ -4,13 +4,27 @@
  * Middleware responsible for validating a token using the "system"  user
  * has a user id that matches the configured system user id
  */
+var _ = require('lodash');
+
 module.exports = (logger) => {
+    var adminIds = {};
+
+    _(process.env.SYSTEM_USER_ID.split(',')).each(id => {
+        adminIds[id.trim()] = true; 
+    });
     
     return (req, resp, next) => {
-        if(req.authUser && req.authUser.handle.toLowerCase() == 'system' && req.authUser.userId != process.env.SYSTEM_USER_ID) {
-            resp.status(400).send({
-                message: 'Invalid User: User "system"  is reserved!' 
-            });
+        if(req.authUser && req.authUser.userId) {
+            if(adminIds[req.authUser.userId]) {
+                req.authUser.handle = 'system';
+                next();
+            } else if (req.authUser.handle.toLowerCase() === 'system') {
+                resp.status(400).send({
+                    message: 'Invalid User: User "system"  is reserved!' 
+                });
+            } else {
+                next();
+            }
         } else {
             next();
         }
