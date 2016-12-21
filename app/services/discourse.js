65af890f4e7b50ebf5d2fc7b636c0a835f78872b
Parth Shah <parth@topcoder.com>
2016-12-21 10:47:35 -0800

    adding more logs

--- app/services/discourse.js
+++ app/services/discourse.js
@@ -73,11 +73,11 @@ var Discourse = (logger) => {
         raw: post
       }
     })
-    .catch((err) => {
-      logger.error('Error creating topic')
-      logger.error(err)
-      return Promise.reject(err)
-    })
+    // .catch((err) => {
+    //   logger.error('Error creating topic')
+    //   logger.error(err)
+    //   return Promise.reject(err)
+    // })
   }
 
   /**
