c6f7b9d7063add650058984dfabcc93b7ff9dbb4
Matthew Twomey <mtwomey@beakstar.com>
2017-07-28 16:21:27 -0500

    Fix issue with >20 comments on a project you're not a member of.
    This problem was caused by the backend not checking to see if the requesting user was an admin and then making the call as the system user. This is how the getTopics work, so the same logic was applied here.

--- app/routes/posts/get.js
+++ app/routes/posts/get.js
@@ -5,6 +5,8 @@ const util = require('tc-core-library-js').util(config);
 const Discourse = require('../../services/discourse');
 const errors = require('common-errors');
 const Adapter = require('../../services/adapter');
+const _  = require('lodash');
+const { USER_ROLE } = require('../../constants');
 
 /**
  * Creates a new post to a topic in Discourse
@@ -21,7 +23,14 @@ module.exports = db => (req, resp, next) => {
   }
 
   const postIds = req.query.postIds.split(',');
-  return discourseClient.getPosts(req.authUser.userId.toString(), req.params.topicId, postIds)
+
+  // Get the posts as the system user if the logged is user is an admin
+  let effectiveUserId = req.authUser.userId.toString();
+  if (_.intersection([USER_ROLE.TOPCODER_ADMIN, USER_ROLE.MANAGER], req.authUser.roles).length > 0) {
+      effectiveUserId = config.get('discourseSystemUsername');
+  }
+
+  return discourseClient.getPosts(effectiveUserId, req.params.topicId, postIds)
       .then((response) => {
         logger.info('Fetched posts from discourse');
         return adapter.adaptPosts(response.data);
