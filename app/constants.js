b7b5e1bc441a3b51d1662dc40016f83e1a9a541a
vikasrohit <vikas.agarwal@topcoder.com>
2018-02-28 09:53:25 +0530

    Revert "Revert "adding notifications prefix in topic name to decouple it from event bus.""

--- app/constants.js
+++ app/constants.js
@@ -19,10 +19,10 @@ module.exports = {
   },
 
   BUS_API_EVENT: {
-    TOPIC_CREATED: 'connect.project.topic.created',
-    TOPIC_DELETED: 'connect.project.topic.deleted',
-    POST_CREATED: 'connect.project.post.created',
-    POST_UPDATED: 'connect.project.post.edited',
-    POST_DELETED: 'connect.project.post.deleted',
+    TOPIC_CREATED: 'notifications.connect.project.topic.created',
+    TOPIC_DELETED: 'notifications.connect.project.topic.deleted',
+    POST_CREATED: 'notifications.connect.project.post.created',
+    POST_UPDATED: 'notifications.connect.project.post.edited',
+    POST_DELETED: 'notifications.connect.project.post.deleted',
   },
 };
